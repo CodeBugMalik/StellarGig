5d431c84309d7f4e4fbfedf3e2c75526c0646c63
CodeBugMalik <pranjalmalik62@gmail.com>
2026-07-03 17:05:11 +0530

    feat(monitoring): setup Sentry Next.js configuration and error logging wrapper

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
+const { withSentryConfig } = require("@sentry/nextjs");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-module.exports = nextConfig;
+
+module.exports = withSentryConfig(
+  nextConfig,
+  {
+    silent: true,
+    org: "stellargig",
+    project: "javascript-nextjs",
+  },
+  {
+    widenClientSandbox: true,
+    transpileClientSDK: true,
+    tunnelRoute: "/monitoring",
+    hideSourceMaps: true,
+    disableLogger: true,
+    disableServerWebpackPlugin: true, // Disable Sentry webpack plugins in local builds to avoid auth errors
+    disableClientWebpackPlugin: true,
+  }
+);
